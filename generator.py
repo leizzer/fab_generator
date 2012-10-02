c381bbe670a135b460b35cbca37732ae7cc5baed
leizzer <lizzydw@gmail.com>
2012-10-02 13:59:05 -0300

    env

--- generator.py
+++ generator.py
@@ -1,24 +1,55 @@
 class Generator:
 
   def __init__(self):
-    self.boolean_question("Create fabfile?")
+    if self.boolean_question("Create fabfile?"):
+      self.create_fab_file()
+      self.generation_steps()
 
   def boolean_question(self, message):
     ans = raw_input(message + " (y/N): ")
     return True if ans == "y" else False
 
+  def string_question(self, message):
+    ans = raw_input(message + ": ")
+    return ans
+
+  def add(self, text):
+    self.file.write("\n" + text)
+
   def create_fab_file(self):
     self.file = open('fabfile.py', 'wr')
-    self.file.write(__head)
+    self.file.write(self.__head)
+
+  def generation_steps(self):
+    self.add(
+        self.__hosts(self.string_question('Host (ex: your.domain.com:port)'))
+        )
+
+    self.add(
+        self.__user(self.string_question('User (ex: root)'))
+        )
+
+    self.add(
+        self.__port(self.string_question('Port'))
+        )
 
   # private
-  __head = """
-          #!/usr/bin/env python
-          from __future__ import with_statement
-          from fabric.api import *
-          from fabric.contrib.contrib import confirm
+  def __hosts(self, text):
+    return "env.hosts = ['{0}']".format(text)
 
-          import datetime
-          """
+  def __user(self, text):
+    return "env.user = '{0}'".format(text)
+
+  def __port(self, text):
+    return "env.port = '{0}'".format(text)
+
+  __head = """\
+#!/usr/bin/env python
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.contrib import confirm
+
+import datetime
+"""
 
 gen = Generator()
