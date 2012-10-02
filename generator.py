87e5362e8497acd3e98d38d0ec71b82e552ff35a
leizzer <lizzydw@gmail.com>
2012-10-02 16:24:45 -0300

    last

--- generator.py
+++ generator.py
@@ -22,26 +22,41 @@ class Generator:
 
   def generation_steps(self):
     self.add(
-        self.__hosts(self.string_question('Host (ex: your.domain.com:port)'))
+        self.__concat_hosts(self.string_question('Host (ex: your.domain.com:port)'))
         )
 
     self.add(
-        self.__user(self.string_question('User (ex: root)'))
+        self.__concat_user(self.string_question('User (ex: root)'))
         )
 
     self.add(
-        self.__port(self.string_question('Port'))
+        self.__concat_port(self.string_question('Port'))
+        )
+
+    self.svn_repo = self.string_question('SVN URL (ex: http://domain.com:port/repo)')
+    self.svn_user = self.string_question('SVN username')
+
+    self.destination = self.string_question('Destination directory on server (ex: /home/wwwdata)')
+    self.project_name = self.string_question('Project name (ex: scan.estudio.com)')
+
+    # 0: svn_user, 1: svn_repo, 2: destination, 3: project_name
+    self.add(
+        self.__concat_body([self.svn_user, self.svn_repo,
+          self.destination, self.project_name])
         )
 
   # private
-  def __hosts(self, text):
+  def __concat_hosts(self, text):
     return "env.hosts = ['{0}']".format(text)
 
-  def __user(self, text):
+  def __concat_user(self, text):
     return "env.user = '{0}'".format(text)
 
-  def __port(self, text):
+  def __concat_port(self, text):
     return "env.port = '{0}'".format(text)
+
+  def __concat_body(self, array):
+    return self.__body.format(*array)
 
   __head = """\
 #!/usr/bin/env python
@@ -50,6 +65,41 @@ from fabric.api import *
 from fabric.contrib.contrib import confirm
 
 import datetime
+"""
+  __body = """\
+def deploy():
+  local('mkdir svn_export')
+  with lcd('svn_export'):
+    local('svn export --username={0} {1} current')
+    local('tar -cvf current.tar current')
+    local('scp -P ' + env.port +' current.tar '+ env.user +'@'+ env.host +':~/')
+
+  # On server side
+  with cd('~'):
+    run('tar -xvf current.tar')
+    run('mv current {2}')
+
+  with cd('{2}/current'):
+    # Your extra code on the server side
+    # Ex:
+    # run('./config')
+    # run('make')
+    # run('make install')
+
+  # Backup & switch on the server
+  with cd('{2}'):
+    new_name = '{3}.'+ datetime.datetime.now().strftime('%Y%m%d.%H%M')
+    run('mv current '+ new_name)
+    run('rm {3}')
+    run('ln -s '+ new_name +' {3}')
+
+  # Extra commands
+  # Ex:
+  # run('sudo /etc/init.d/nginx stop')
+
+  #Claning
+  local('rm -rf svn_export')
+  run('rm -rf ~/current.tar')
 """
 
 gen = Generator()
